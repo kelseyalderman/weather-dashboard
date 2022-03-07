4137c0d5efc692972e80fb8186427641aa1ddea5
Kelsey Alderman <kelseyalderman@gmail.com>
2022-03-06 22:37:58 -0800

    form submit handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
-var currentWeatherTitle = document.querySelector("#current-weather-title");
+var cityTitleEl = document.querySelector("#current-weather-title");
 var forecastTitle = document.querySelector("#forecast-title");
+
+var getCityWeather = function (city) {
+  // format OpenWeather api url
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial";
+
+  // make a request to url
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to OpenWeather");
+    });
+};
 
 var formSubmitHandler = function (event) {
   // prevent page from refreshing
@@ -14,39 +38,19 @@ var formSubmitHandler = function (event) {
   if (cityName) {
     getCityWeather(cityName);
 
-    // clear old content
-    weatherContainerEl.textContent = "";
-    cityInputEl.value = "";
+    // clear the search input
+    $("#city").val("");
   } else {
     alert("Please enter a city");
   }
 };
 
-var getCityWeather = function (city) {
-  // format OpenWeather api url
-  var apiUrl =
-    "https://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
-    "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial";
-
-  // make a request to url
-  fetch(apiUrl)
-    .then(function (response) {
-      // request was successful
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-        });
-      } else {
-        alert("Error: " + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert("Unable to connect to OpenWeather");
-    });
-};
-
 var displayWeather = function (weatherData) {
-  
+  // format and display data
+  console.log(weatherData);
+  console.log("test");
+  $("#current-weather-title").text("test");
 };
+
+// add event listeners to forms
+$("#search").on("click", formSubmitHandler);
